54c8dab095270b5ec6c5ac063498556e3bc57b25
shx <shx@dev.matfor.local>
2024-07-20 01:51:30 +0000

    added scheduler for compress and encrypt

--- compress-and-encrypt/compress_and_encrypt.sh
+++ compress-and-encrypt/compress_and_encrypt.sh
@@ -12,15 +12,12 @@ else
     encrypt_option="-P $COMPRESS_PASSWORD"
 fi
 
-# Get the current date
-current_date=$(date +%Y-%m-%d)
-current_time=$(date +%H-%M-%S)
-
 # Compress and encrypt each folder found in the input directory
 for folder in "$input_dir"*/; do
     # Remove trailing slash for folder name
     folder_name=$(basename "$folder")
-
+    current_date=$(date +%Y-%m-%d)
+    current_time=$(date +%H-%M-%S)
     output_zip="${output_dir}${current_date}_${current_time}-backup-${folder_name}.zip"
 
     if [ -z "$encrypt_option" ]; then
